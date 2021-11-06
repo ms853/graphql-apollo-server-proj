2e227b4286f929be8010cdb58f61b9f0a6795667
Mike <msakyiama51@gmail.com>
2021-11-06 17:03:16 +0000

    Created a mock resolver for my gql query.

--- gql-ts-app/src/index.ts
+++ gql-ts-app/src/index.ts
@@ -1,7 +1,7 @@
 import { ApolloServer, gql } from 'apollo-server';
-import * as sessionDataset from '../data/sessions.json';
-const typeDefs = gql`
+import * as sessions from '../data/sessions.json';
 
+const typeDefs = gql`
 type Query {
     sessions: [Session]
 }     
@@ -17,13 +17,17 @@ type Session {
     format: String,
     track: String,
     level: String
-}
-`
-const resolver : any = {
-    sessions: sessionDataset 
+}`
+
+const resolvers = {
+    Query: {
+        sessions: function(): any[] {
+            return sessions;
+        }
+    }
 };
    
-const server : ApolloServer = new ApolloServer({ typeDefs });
+const server : ApolloServer = new ApolloServer({ typeDefs, resolvers });
 
 server.listen({ port: process.env.PORT || 4000 })
 .then((url) => { console.log(`graphql server is running on ${url.port}`)}); 
